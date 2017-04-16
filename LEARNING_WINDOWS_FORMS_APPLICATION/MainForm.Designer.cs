7dfba0c28ba9195da9ca98e1434fb1ab1f0f3e26
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2017-04-16 17:50:53 +0430

    Add project files.

---
+++ LEARNING_WINDOWS_FORMS_APPLICATION/MainForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace LEARNING_WINDOWS_FORMS_APPLICATION
+{
+	partial class MainForm
+	{
+		private System.ComponentModel.IContainer components = null;
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(12, 41);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(12, 70);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(12, 99);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "button4";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button_Click);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(12, 128);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(75, 23);
+			this.button5.TabIndex = 4;
+			this.button5.Text = "button5";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.Button_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 157);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 23);
+			this.textBox1.TabIndex = 5;
+			this.textBox1.Click += new System.EventHandler(this.Button_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.SteelBlue;
+			this.ClientSize = new System.Drawing.Size(379, 321);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Main";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
---
+++ LEARNING_WINDOWS_FORMS_APPLICATION/MainForm.cs
@@ -0,0 +1,82 @@
+﻿namespace LEARNING_WINDOWS_FORMS_APPLICATION
+{
+	public partial class MainForm : System.Windows.Forms.Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, System.EventArgs e)
+		{
+			// currentButton:
+			// دکمه جاری که بر روی آن کلیک کرده‌ایم
+
+			// Solution (1)
+			//System.Windows.Forms.Button
+			//	currentButton = (System.Windows.Forms.Button)sender;
+			// /Solution (1)
+
+			// Solution (2)
+			//System.Windows.Forms.Button currentButton = null;
+
+			//if (sender is System.Windows.Forms.Button)
+			//{
+			//	currentButton =
+			//		(System.Windows.Forms.Button)sender;
+			//}
+			//else
+			//{
+			//	System.Windows.Forms.MessageBox.Show("Note: [sender] is not Button!");
+
+			//	return;
+			//}
+			// /Solution (2)
+
+			// Solution (3)
+			System.Windows.Forms.Button
+				currentButton = sender as System.Windows.Forms.Button;
+
+			if (currentButton == null)
+			{
+				System.Windows.Forms.MessageBox.Show("Note: [sender] is not Button!");
+
+				return;
+			}
+			// /Solution (3)
+
+			switch (currentButton.Name)
+			{
+				case "button1":
+				{
+					System.Windows.Forms.MessageBox.Show("Hello, World (1)!");
+					break;
+				}
+
+				case "button2":
+				{
+					System.Windows.Forms.MessageBox.Show("Hello, World (2)!");
+					break;
+				}
+
+				case "button3":
+				{
+					System.Windows.Forms.MessageBox.Show("Hello, World (3)!");
+					break;
+				}
+
+				case "button4":
+				{
+					System.Windows.Forms.MessageBox.Show("Hello, World (4)!");
+					break;
+				}
+
+				case "button5":
+				{
+					System.Windows.Forms.MessageBox.Show("Hello, World (5)!");
+					break;
+				}
+			}
+		}
+	}
+}
